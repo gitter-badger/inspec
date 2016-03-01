396f336ec9634884aa95f30d6f98a8a9780a7cb7
Christoph Hartmann <chris@lollyrock.com>
2016-03-01 21:02:47 +0100

    add vbscript resource

---
+++ lib/resources/vbscript.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+# author: Christoph Hartmann
+# author: Dominik Richter
+
+# we cannot use script control, because it works on 32 bit systems only
+# $script = new-object -comobject MSScriptControl.ScriptControl
+# $script.language = "vbscript"
+# $script.ExecuteStatement($Cmd)
+class VBScript < Script
+  name 'vbscript'
+  desc ''
+  example "
+    script = <<-EOH
+      # you vbscript
+    EOH
+
+    describe vbscript(script) do
+      its('stdout') { should eq 'output' }
+    end
+  "
+
+  def initialize(vbscript)
+    return skip_resource 'The `cscript` resource is not supported on your OS yet.' unless inspec.os.windows?
+
+    cmd = <<-EOH
+$vbscript = @"
+#{vbscript}
+"@
+
+# create temp file
+$filename = [System.IO.Path]::GetTempFileName() + ".vbs"
+New-Item $filename -type file -force -value $vbscript  | Out-Null
+
+# run cscript with nologo option
+cscript.exe /nologo $filename
+
+EOH
+    puts cmd
+    super(cmd)
+  end
+
+  def to_s
+    'Windows VBScript'
+  end
+end
